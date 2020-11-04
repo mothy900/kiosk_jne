f01a1e4f8b59511b636b1523d6e7c5bb0b8346fd
mothy900 <mothy900@naver.com>
2020-11-05 00:46:54 +0900

    first commit basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./reset.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="kiosk__title">
+        <span className="kiosk_-title title">Title</span>
+      </div>
+      <div className="kiosk__image">
+        <img src="https://i.pinimg.com/564x/5c/d6/d7/5cd6d781e92d4ed0bf7615e97cda9b34.jpg"></img>
+      </div>
+      <div className="kiosk__footer">
+        <div className="kiosk__btn">
+          <button className="kiosk__btn1">Click</button>
+          <span>1번 공정 데이터</span>
+        </div>
+        <div className="kiosk__btn">
+          <button className="kiosk__btn2">Click</button>
+          <span>2번 공정 데이터</span>
+        </div>
+        <div className="kiosk__btn">
+          <button className="kiosk__btn3">Click</button>
+          <span>3번 공정 데이터</span>
+        </div>
+      </div>
     </div>
   );
 }
